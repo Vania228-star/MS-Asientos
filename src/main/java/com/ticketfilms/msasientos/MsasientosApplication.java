361b3d1b19fb454f907dec50970c30b084f5d0ef
Vania <Van.carvajal@duocuc.cl>
2026-08-26 17:53:32 -0400

    first commit ms-asientos

---
+++ src/main/java/com/ticketfilms/msasientos/MsasientosApplication.java
@@ -0,0 +1,13 @@
+package com.ticketfilms.msasientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsasientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsasientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketfilms/msasientos/MsasientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketfilms.msasientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsasientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
