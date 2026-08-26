41b48dc3ec14c6087de4bf107803b12d6f0b99ce
Vania <Van.carvajal@duocuc.cl>
2026-08-26 19:00:15 -0400

    Repository encargado buscar reservas por el ID del usuario, ReservaRepository

--- src/main/java/com/ticketfilms/msasientos/repository/ReservaRepository.java
+++ src/main/java/com/ticketfilms/msasientos/repository/ReservaRepository.java
@@ -1,5 +1,14 @@
 package com.ticketfilms.msasientos.repository;
 
-public class ReservaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ticketfilms.msasientos.model.Reserva;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long>{
     
+    List<Reserva> findByUsuarioId(String usuarioId);
 }
