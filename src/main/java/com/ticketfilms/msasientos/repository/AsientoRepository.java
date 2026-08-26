cb5b0ac73e646871c9192aacb68a2d30bc1fa4fd
Vania <Van.carvajal@duocuc.cl>
2026-08-26 18:54:53 -0400

    Repository encargado de gestionar los asientos de la sala, AsientoRepository

--- src/main/java/com/ticketfilms/msasientos/repository/AsientoRepository.java
+++ src/main/java/com/ticketfilms/msasientos/repository/AsientoRepository.java
@@ -1,5 +1,14 @@
 package com.ticketfilms.msasientos.repository;
 
-public class AsientoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ticketfilms.msasientos.model.Asiento;
+
+@Repository
+public interface AsientoRepository extends JpaRepository<Asiento, Long>{
     
+    List<Asiento> findByFuncionId(Long funcionId);
 }
