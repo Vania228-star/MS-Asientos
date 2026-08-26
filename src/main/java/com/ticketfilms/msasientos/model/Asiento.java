ff18966fc1c71f5a6aecc404e43df3e8dc9b5cf2
Vania <Van.carvajal@duocuc.cl>
2026-08-26 18:33:58 -0400

    Datos de la clase Asiento

--- src/main/java/com/ticketfilms/msasientos/model/Asiento.java
+++ src/main/java/com/ticketfilms/msasientos/model/Asiento.java
@@ -1,5 +1,28 @@
 package com.ticketfilms.msasientos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name= "Asientos")
+@Data
 public class Asiento {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "funcion_id", nullable = false)
+    private Long funcionId;
+
+    @Column(name= "numero-asiento", nullable = false)
+    private String numeroAsiento;
+
+    @Column(name= "estado", nullable = false)
+    private String estado;
 }
