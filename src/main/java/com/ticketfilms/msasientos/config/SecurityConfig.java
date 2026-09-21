adae85aed02aa7474cb2d6be9ecc45d9d05c51fb
oriana02 <or.solorzano@duocuc.cl>
2026-09-21 16:28:58 -0300

    fix: securityConfig

--- src/main/java/com/ticketfilms/msasientos/config/SecurityConfig.java
+++ src/main/java/com/ticketfilms/msasientos/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ticketfilms.msasientos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/asientos/sala").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
